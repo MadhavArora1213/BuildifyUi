87d2d2346c99d4ba1e18f057cab2d7c3f2c8576c
MadhavArora1213 <madhavarora132005@gmail.com>
2025-01-26 10:47:39 +0530

    Add SimpleToggle Switch

---
+++ src/App3.tsx
@@ -0,0 +1,20 @@
+import type React from "react";
+import { Suspense, lazy } from "react";
+import { ThemeProvider } from "./lib/theme/ThemeContext"; // Adjust the path
+import SimpleToggle from "./registry/Components/ToggleSwitches/SimpleToggle";
+const Navbar = lazy(() => import("./components/core/Navbar"));
+
+
+const App3: React.FC = () => {
+  return (
+    <ThemeProvider>
+      {/* Suspense to handle the loading state for lazy-loaded components */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar />
+      <SimpleToggle/>
+      </Suspense>
+    </ThemeProvider>
+  );
+};
+
+export default App3;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App2 from './App2.tsx';
+import App3 from './App3.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App2 />
+    <App3 />
   </StrictMode>
 );
