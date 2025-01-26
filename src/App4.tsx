020a49002f14abecc6cf4564bb337cea7050a1b8
MadhavArora1213 <madhavarora132005@gmail.com>
2025-01-26 12:20:33 +0530

    Add  Switch Radio button

--- src/App4.tsx
+++ src/App4.tsx
@@ -2,21 +2,27 @@ import type React from "react";
 import { Suspense, lazy } from "react";
 import { ThemeProvider } from "./lib/theme/ThemeContext"; // Adjust the path
 import VehicleSelector from "./registry/Components/RadioButtons/VehicleSelector";
+import Star from "./registry/Components/RadioButtons/Star";
+import AnimatedStar from "./registry/Components/RadioButtons/AnimatedStar";
+import SwitchRadio from "./registry/Components/RadioButtons/SwitchRadio";
 
 // Lazy load components
 
 const Navbar = lazy(() => import("./components/core/Navbar"));
 
 const App4: React.FC = () => {
-  return (
-    <ThemeProvider>
-      {/* Suspense to handle the loading state for lazy-loaded components */}
-      <Suspense fallback={<div>Loading...</div>}>
-     <Navbar/>
-     <VehicleSelector/>
-      </Suspense>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider>
+            {/* Suspense to handle the loading state for lazy-loaded components */}
+            <Suspense fallback={<div>Loading...</div>}>
+                <Navbar />
+                <VehicleSelector />
+                <Star />
+                <AnimatedStar/>
+                <SwitchRadio/>
+            </Suspense>
+        </ThemeProvider>
+    );
 };
 
 export default App4;
