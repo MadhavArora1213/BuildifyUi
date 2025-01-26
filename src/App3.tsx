ed5118a294e200b9ba249c94517c49f518ebbb68
MadhavArora1213 <madhavarora132005@gmail.com>
2025-01-26 11:02:00 +0530

    Add Emoji Toggle Switch

--- src/App3.tsx
+++ src/App3.tsx
@@ -5,6 +5,7 @@ import SimpleToggle from "./registry/Components/ToggleSwitches/SimpleToggle";
 import GearToggle from "./registry/Components/ToggleSwitches/GearToggle";
 import LoveToggle from "./registry/Components/ToggleSwitches/LoveToggle";
 import DownloadToggle from "./registry/Components/ToggleSwitches/DownloadToggle";
+import EmojiToggle from "./registry/Components/ToggleSwitches/EmojiToggle";
 const Navbar = lazy(() => import("./components/core/Navbar"));
 
 
@@ -14,10 +15,11 @@ const App3: React.FC = () => {
       {/* Suspense to handle the loading state for lazy-loaded components */}
       <Suspense fallback={<div>Loading...</div>}>
         <Navbar />
-      <SimpleToggle/>
-      <GearToggle/>
-      <LoveToggle/>
-      <DownloadToggle/>
+        <SimpleToggle />
+        <GearToggle />
+        <LoveToggle />
+        <DownloadToggle />
+        <EmojiToggle/>
       </Suspense>
     </ThemeProvider>
   );
