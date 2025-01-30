5d6fc0d1ebc6f211286066d2a651c858cd3619f9
MadhavArora1213 <madhavarora132005@gmail.com>
2025-01-30 21:10:23 +0530

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App6 from './App6.tsx';
+import App7 from './App7.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App6 />
+    <App7 />
   </StrictMode>
 );
