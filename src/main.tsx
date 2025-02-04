a40401df492946acf4f7fa9d00d5d4707a28c155
MadhavArora1213 <madhavarora132005@gmail.com>
2025-02-04 22:24:05 +0530

    update file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App7 from './App7.tsx';
-import './index.css';
+import './index.css'; // index.css
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
